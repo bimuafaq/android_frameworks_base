8852e2dd5e8d878a2c9a525c323e2a1d3f16280a
Anushek Prasal <anushekprasal@gmail.com>
2020-07-07 00:19:10 +0530

    base: Allow to show daily data usage [1/2]
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4829,6 +4829,14 @@ public final class Settings {
         public static final String SMART_CHARGING_RESUME_LEVEL = "smart_charging_resume_level";
 
         /**
+         * Data usage period
+         * 0: Daily
+         * 1: Monthly
+         * @hide
+         */
+        public static final String DATA_USAGE_PERIOD = "data_usage_period";
+
+        /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
          *
