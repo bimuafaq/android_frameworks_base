67997c13e24a8e035d57ca626fce05ef6259c5b0
Rituj Beniwal <ritujbeniwal@gmail.com>
2021-06-28 09:44:01 +0700

    SystemUI: Brightness Slider: Beautify and fix bugs
    * Move media player between brightness slider and footer button
    * Fix behaviour on landscape
    * Allow user more notification space in landscape
    * Fix brightness slider behaviour when swiping the QS panel or when swiping to show the statusbar
    * Switch OP footerview to new parent when switching tile layout
    * And some more
    
    Signed-off-by: Rituj Beniwal <ritujbeniwal@gmail.com>
    Change-Id: I11622eeba980fc8eb01c45f54c6ba4fae9a4a851

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1300,6 +1300,7 @@ public class StatusBar extends SystemUI implements DemoMode,
                 if (qs instanceof QSFragment) {
                     mQSPanel = ((QSFragment) qs).getQsPanel();
                     mQSPanel.setBrightnessMirror(mBrightnessMirrorController);
+                    ((QuickStatusBarHeader)qs.getHeader()).getHeaderQsPanel().setBrightnessMirror(mBrightnessMirrorController);
                 }
             });
         }
